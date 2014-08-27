411c8f5e23025b11dcadb71a89ba3052274b8a7e
Nick Cherry <nickcherryjiggz@gmail.com>
2014-08-27 11:00:11 -0500

    allowing 127.0.0.1 requests, in addition to localhost

--- background.js
+++ background.js
@@ -1,5 +1,7 @@
 chrome.webRequest.onBeforeRequest.addListener(function(details) {
-  if (/^.*\.(css)/.test(details.url) && !/localhost/.test(details.url)) {
+  if (/^.*\.(css)/.test(details.url) &&
+      !/localhost/.test(details.url) &&
+      !/127.0.0.1/.test(details.url)) {
     return { cancel: true };
   }
 }, {
